72eaaf5d4b9ac9ead9c9de59c189e848b356989e
Francisco Costa <francisco100eg@gmail.com>
2023-07-30 09:21:54 -0300

    Feat: Function structure

--- App.tsx
+++ App.tsx
@@ -1,7 +1,36 @@
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+
+import * as Yup from 'yup';
+
+const passwordSchema = Yup.object().shape({
+  passwordLenght: Yup.number()
+    .min(8, 'Password precisa ter no mínimo 8 caracteres')
+    .max(16, 'Password pode ter no máximo 16 caracteres')
+    .required('Password é obrigatório'),
+});
 
 export default function App() {
+  const [password, setpassword] = useState<string>('');
+  const [isPasswordGenerated, setisPasswordGenerated] =
+    useState<boolean>(false);
+  const [lowercase, setlowercase] = useState<boolean>(true);
+  const [uppercase, setuppercase] = useState<boolean>(false);
+  const [numbers, setnumbers] = useState<boolean>(false);
+  const [symbols, setsymbols] = useState<boolean>(false);
+
+  const generatePasswordString = (passwordLenght: number) => {
+    return;
+  };
+
+  const createPassword = async (characters: string, passwordLenght: number) => {
+    return;
+  };
+
+  const resetPasswordState = () => {
+    return;
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.headerTitle}>Password Generator</Text>
@@ -12,13 +41,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d5dce7',
+    backgroundColor: '#13414d',
     alignItems: 'center',
   },
   headerTitle: {
     fontSize: 30,
     fontWeight: '700',
-    color: '#6b7688',
+    color: '#e9f0ed',
     marginTop: 20,
     textShadowColor: 'rgba(0, 0, 0, 0.2)',
     textShadowOffset: {width: 2, height: 1},
